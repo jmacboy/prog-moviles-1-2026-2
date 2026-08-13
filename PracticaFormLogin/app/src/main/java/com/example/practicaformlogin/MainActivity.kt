8a1ffba9f04b8ff8ce60cf1a52e85714ff8c0e8f
José Miguel Alvarez <j.m.alvarez.camacho@gmail.com>
2026-08-13 14:54:42 -0400

    actualizado login y registro

--- PracticaFormLogin/app/src/main/java/com/example/practicaformlogin/MainActivity.kt
+++ PracticaFormLogin/app/src/main/java/com/example/practicaformlogin/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -45,46 +44,77 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FormLogin(modifier: Modifier = Modifier) {
+    var registro by rememberSaveable { mutableStateOf(false) }
+    Column {
+        Button(
+            onClick = { registro = !registro },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = if (registro) "Ir a login" else "Ir a registro")
+        }
+        if (registro) {
+            PantallaRegistro(modifier)
+        } else {
+            PantallaLogin(modifier)
+        }
+        /*
+        * TAREA: En la pantalla de registro, colocar un formulario que incluya el nombre en uno de los campos.
+        * Agregar el usuario registrado a una lista (una variable nomas).
+        * Que de esa lista se verifique cuando se haga un login.
+        */
+
+    }
+}
+
+@Composable
+fun PantallaRegistro(modifier: Modifier = Modifier) {
+    Text("Pantalla de registro aqui")
+
+}
+
+@Composable
+fun PantallaLogin(modifier: Modifier = Modifier) {
+    var username by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
     val context = LocalContext.current
+
     Column(
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxSize()
             .padding(16.dp)
     ) {
-        var username by rememberSaveable { mutableStateOf("") }
-        var password by rememberSaveable { mutableStateOf("") }
-
+        Text(text = "Iniciar sesión", modifier = Modifier.padding(bottom = 16.dp))
         OutlinedTextField(
             value = username,
-            onValueChange = {
-                username = it
-            },
+            onValueChange = { username = it },
             label = { Text("Usuario") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
+            modifier = Modifier.fillMaxWidth()
         )
         OutlinedTextField(
             value = password,
-            onValueChange = {
-                password = it
-            },
+            onValueChange = { password = it },
             label = { Text("Contraseña") },
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
-        Button(
-            onClick = {
-                if (username == "admin" && password == "admin") {
-                    Toast.makeText(context, "Login exitoso", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(context, "Login fallido", Toast.LENGTH_SHORT).show()
-                }
-            },
             modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Iniciar sesión")
+        )
+        Row(modifier = Modifier.padding(top = 16.dp)) {
+            Button(
+                onClick = {
+                    if (username == "admin" && password == "admin") {
+                        Toast.makeText(context, "Inicio de sesión exitoso", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Usuario o contraseña incorrectos",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Iniciar sesión")
+            }
         }
     }
 }
